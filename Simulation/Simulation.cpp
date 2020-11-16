a9011428305f684e4564eb8eccc1ff96d793dbb2
Giovanni Campaner <giocampaner@gmail.com>
2020-11-15 21:49:13 -0300

    - Checkpoint

--- Simulation/Simulation.cpp
+++ Simulation/Simulation.cpp
@@ -4,8 +4,40 @@
 
 #include "Simulation.hpp"
 
+static auto constructCNN() -> void
+{
+    using namespace tiny_dnn;
+
+    network<sequential> net{};
+
+    // add layers
+    net << layers::fc(6,10) << activation::tanh()
+        << layers::fc(10,10) << activation::tanh()
+        << layers::fc(10,10) << activation::tanh()
+        << layers::fc(10,4);
+
+    assert(net.in_data_size() == 10);
+    assert(net.out_data_size() == 4);
+
+    {
+        std::vector<vec_t> data{ { 1, 0 }, { 0, 2 } };
+        std::vector<vec_t>  out{ { 2 },    { 1 } };
+
+        // declare optimization algorithm
+        gradient_descent optimizer{};
+
+        // train (1-epoch, 30-minibatch)
+        net.fit<mse, gradient_descent>(optimizer, data, out, 30, 1);
+    }
+
+    // save
+    net.save("C:/Users/Giovanni/Desktop/auto2/net.json", content_type::weights_and_model, file_format::json);
+}
+
 auto Simulation::init() -> void
 {
+    constructCNN();
+
     this->ground = createGround(&world);
     this->maze.init(&world, ground);
     this->cars = std::vector<Car>{ 100 };
